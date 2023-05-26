2bcbfe46434adb2f3415bc39c332c21dd2a2f27b
Courtney Sherman <csherman177@gmail.com>
2023-05-25 19:15:28 -0600

    changes

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# logoCraft
+## Description 
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+## Project URL
+  https://github.com/csherman177/logoCraft   
+
+## Deployment
+  https://csherman177.github.io/logoCraft/
+
+## Demo/Screenshots
+  <table>
+  <tr>
+    <td>SVG Logo Generator Screenshot</td>
+  </tr>
+  <tr>
+    <td><img src= "logoScreenshot.png" height=300 alt="SVG Logo Screenshot"></td>
+  </tr>
+  </table>
+
+  <table>
+  <tr>
+    <td>SVG Logo Generator Video</td>
+  </tr>
+  <tr>
+    <td><img src= "SVGDemo.webm" height=300 alt="SVG Logo Demo"></td>
+  </tr>
+  </table>
+ 
+  ## Contact
+  Email: csherman177@gmail.com
+
+  ## Author
+  Author(s): Courtney Sherman 
+  GitHub: https://github.com/csherman177/ 
--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 // Inquirer (node package manager) import
-
 const inquirer = require('inquirer');
-const fs  = require ('fs');
-const shapes = require('./lib/shapes.js');
-const { SVG, setShape } = require('./lib/svg.js');
-
+const fs = require('fs');
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
+const setShape = require('./lib/generateShape.js')
 
 const questions = [
-    // empty string to hold letters
-    {
-        type: 'input',
-        message: 'What 3 letters would you like to appear on LOGO?',
-        name: 'text'
-    },
-    // how do I link the colors to the project?
-    {
-        type: 'input',
-        message: 'Which color would you like your text to be?',
-        name: 'txtcolor'
-    },    
-      // how do I link the colors to the project?
-    {
-        type: 'input',
-        message: 'Which color would you like your shape to be?',
-        name: 'color'
-    },
-    // what do I map the shapes too?
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Which would you like to use?',
-        choices: ['circle', 'triangle', 'square']
-       
-    },
+  // empty string to hold letters
+  {
+    type: 'input',
+    message: 'What 3 letters would you like to appear on LOGO?',
+    name: 'text'
+  },
+  // how do I link the colors to the project?
+  {
+    type: 'input',
+    message: 'Which color would you like your text to be?',
+    name: 'txtcolor'
+  },
+  // how do I link the colors to the project?
+  {
+    type: 'input',
+    message: 'Which color would you like your shape to be?',
+    name: 'color'
+  },
+  // what do I map the shapes too?
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Which shape would you like to use?',
+    choices: ['circle', 'triangle', 'square']
+  },
 ];
 
-
 function writeToFile(fileName, data) {
-  if (typeof data === 'undefined') {
-    console.log('Error: data is undefined');
-    return;
-  }
-  const bufferData = Buffer.from(data);
-  fs.writeFile(fileName, bufferData, (err) => {
+  //const bufferData = Buffer.from(JSON.stringify(data));
+  fs.writeFile(fileName, data, (err) => {
     if (err) {
       console.log('Error writing to file:', err);
     } else {
@@ -54,10 +46,8 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((data) => {
     console.log(data);
-    const svg = new SVG();
-    const svgElement = svg.setShape(data);
-    const svgString = svg.render(svgElement);
-    writeToFile('logo.svg', svgString);
+    const template = setShape(data);
+    writeToFile('logo.svg', template);
   });
 }
 
